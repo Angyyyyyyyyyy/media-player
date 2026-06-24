93563bf9f189dc6cf4ea5d6cf82330dd1562b811
Angyyyyyyyyyy <yang.7150@osu.edu>
2026-06-23 22:04:14 -0400

    add makefiles

---
+++ build/CMakeFiles/mediaplayer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mediaplayer.dir/main.cpp.o"
+  "CMakeFiles/mediaplayer.dir/main.cpp.o.d"
+  "mediaplayer"
+  "mediaplayer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mediaplayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mediaplayer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for mediaplayer.
+# This may be replaced when dependencies are built.
